e1a7f1f60e32856a61df8683bfea3621205ea907
Arvind kumar <avigehlot05@gmail.com>
2024-11-23 20:42:12 +0530

    udpayted

--- components/SEO.js
+++ components/SEO.js
@@ -1,16 +1,6 @@
-import { NextSeo } from 'next-seo';
 
 export default function SEO({ title, description }) {
   return (
-    <NextSeo
-      title={title}
-      description={description}
-      openGraph={{
-        title,
-        description,
-        url: 'https://your-site-url.com',
-        type: 'website',
-      }}
-    />
+   <>Hello From SEO</>
   );
 }
