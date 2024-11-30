42ee5a57eb34f2570feed4e4d2f783140c2df4c4
Arvind kumar <avigehlot05@gmail.com>
2024-11-30 19:33:19 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Enables static export
     images: {
         domains: ['via.placeholder.com','res.cloudinary.com'], // Add the domain of the external image here
       },
