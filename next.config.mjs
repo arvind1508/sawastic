d506affe5b548c652f80d9e5ce5fb8513e442f85
Arvind kumar <avigehlot05@gmail.com>
2024-11-30 19:32:00 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,8 @@ const nextConfig = {
     images: {
         domains: ['via.placeholder.com','res.cloudinary.com'], // Add the domain of the external image here
       },
+      basePath: '/sawastic', // Replace with your GitHub repository name
+
 };
 
 export default nextConfig;
